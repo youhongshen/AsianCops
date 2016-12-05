e2db06441aad0d5db77fd63136266a6fda395992
joan <youhongshen@gmail.com>
2016-12-04 19:09:52 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='AsianCops',
+    version='1.0',
+    packages=['home', 'home.models', 'home.migrations', 'search', 'AsianCops', 'AsianCops.settings'],
+    url='',
+    license='',
+    author='joan',
+    author_email='you_hong@yahoo.com',
+    description='Wagtail based web site for Asian Cops'
+)
